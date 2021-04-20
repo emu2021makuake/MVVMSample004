4e11b526ab1de86a1fd8f5d98fe68abfd81e2e97
emu2021makuake <81235941+emu2021makuake@users.noreply.github.com>
2021-04-20 14:59:45 +0900

    Add files via upload

---
+++ MVVMSample004/MVVMSample004/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MVVMSample004.ViewModels;
+using System.Windows;
+
+namespace MVVMSample004.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
